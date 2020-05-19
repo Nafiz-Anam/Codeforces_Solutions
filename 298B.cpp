a4d48d37132cf6cb02d2ee46d6903b7a568178c8
Nafiz Anam <60095272+Nafiz-Anam@users.noreply.github.com>
2020-05-19 22:43:15 +0600

    Update 298B.cpp

--- 298B.cpp
+++ 298B.cpp
@@ -1,1 +1,97 @@
+/////**** In the name of ALLAH ****/////
+/**
+	Kazi Nafiz Anam
+		CSE'19
+**/
+#include<bits/stdc++.h>
+using namespace std;
 
+/*** Optimization ***/
+#pragma GCC optimize("Ofast,no-stack-protector")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC optimize("unroll-loops")
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned long long ull;
+///use for long num in vector
+/// to find max/min dig
+#define     minv(v)         *min_element(v.begin(),v.end())
+#define     maxv(v)         *max_element(v.begin(),v.end())
+
+#define     ios             ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define     pi              acos(-1.00)
+#define     ia(arr,a,n)	    for(int i=a; i<n; i++) cin>>arr[i];
+#define     oa(arr,a,n)	    for(int i=a; i<n; i++) cout<<arr[i]<<" ";
+#define     all(x)          x.begin(),x.end()
+#define     allr(x)         x.begin(),x.end(),greater<int>()
+#define     fill(arr)       memset(arr,0,sizeof(arr))
+#define	    nl              '\n'
+/*** STLs ***/
+typedef vector<ll> vll;
+typedef set<ll> sll;
+typedef multiset<ll> msll;
+typedef queue<ll> qll;
+typedef map<ll,ll> mll;
+typedef pair<ll,ll> pll;
+typedef vector<pair<ll,ll>> vpll;
+
+#define pb push_back
+#define ff first
+#define ss second
+#define mp make_pair
+
+/// *** ============= *** Star from Here *** ============= *** ///
+
+string s;
+int cnt[30];
+
+int main()
+{
+    ios
+    int t, sx, sy, ex, ey, time(-1);
+    string wind;
+    cin >> t >> sx >> sy >> ex >> ey >> wind;
+    for (int i = 0; i < t; ++i)
+    {
+        switch (wind[i])
+        {
+        case 'E':
+            if (sx < ex)
+            {
+                sx += 1;
+            }
+            break;
+        case 'S':
+            if (sy > ey)
+            {
+                sy -= 1;
+            }
+            break;
+        case 'W':
+            if (sx > ex)
+            {
+                sx -= 1;
+            }
+            break;
+        case 'N':
+            if (sy < ey)
+            {
+                sy += 1;
+            }
+            break;
+        default:
+            break;
+        }
+        if (sx == ex && sy == ey)
+        {
+            time = i + 1;
+            break;
+        }
+    }
+    cout << time << endl;
+
+
+    //cout<<"Case "<<cn<<": "<<"good"<<endl;
+    return 0;
+}
